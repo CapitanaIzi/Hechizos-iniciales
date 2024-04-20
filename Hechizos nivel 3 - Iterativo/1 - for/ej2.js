2376b35291a9c1ea7100ecdc033fcd097215f962
Francesca <guizarfrancesca7@gmail.com>
2024-04-19 22:27:53 -0300

    en proceso

--- Hechizos nivel 3 - Iterativo/1 - for/ej2.js
+++ Hechizos nivel 3 - Iterativo/1 - for/ej2.js
@@ -19,27 +19,29 @@ const NOMBRE_HECHIZO_CORRECTO = "Patronus"
 const NOMBRE_ENEMIGO= "DEMENTOR"
 const VIDA_MAXIMA_ENEMIGA = 1000
 const VIDA_MAXIMA_ESTUDIANTE = 350
-const DAÑO_X_TURNO_AL_ESTUDIANTE = 75
-const DAÑO_AL_ENEMIGO_POR_HECHIZO = 267
-const TURNO_TOTALES = 5
-const MSJ_DE_BIENVENIDA = "Protegete de los Dementores, estas son criaturas oscuras que se alimentan de la felicidad de las personas, dejándolas con recuerdos dolorosos y desesperación. Que bueno que estas aprendiendo el hechizo Patronus, este repele a estas criaturas y los aleja con recuerdos felices"
-const MSJ_DE_EXPLICACION = "Si no ingresas el hechizo correctamente, perderás turnos y el Dementor consumirá tu felicidad y recuerdos felices, acercándote más a la derrota. Tu vida maxima inicial es de:"
+const DANIO_X_TURNO_AL_ESTUDIANTE = 75
+const DANIO_AL_ENEMIGO_POR_HECHIZO = 267
+const TURNO_TOTALES = 8
+const MSJ_DE_BIENVENIDA = `Protegete de los Dementores, estas son criaturas oscuras que se alimentan de la felicidad de las personas, dejándolas con recuerdos dolorosos y desesperación. 
+Que bueno que estas aprendiendo el hechizo Patronus, este repele a estas criaturas y los aleja con recuerdos felices. Si no ingresas el hechizo correctamente, perderás turnos y el Dementor 
+consumirá tu felicidad y recuerdos felices, acercándote más a la derrota. Tu vida maxima inicial es de:`
 
 function main() {
     let hechizoLanzadoEstudiante = "error"
     let vidaActualEnemigo= "vida actual enemigo"
     let vidaActualEstudiante= "vida actual del estudiante"
     console.log(MSJ_DE_BIENVENIDA);
-    console.log(MSJ_DE_EXPLICACION, VIDA_MAXIMA_ESTUDIANTE);
+   
     for (let i = 0; i < TURNO_TOTALES; i++) {
         console.log("Ingresa el hechizo; Turnos totales son:", TURNO_TOTALES);
         hechizoLanzadoEstudiante = leer();
         if (hechizoLanzadoEstudiante == NOMBRE_HECHIZO_CORRECTO) {
-            vidaActualEnemigo=VIDA_MAXIMA_ENEMIGA-DAÑO_AL_ENEMIGO_POR_HECHIZO
-            console.log("Lo estas haciendo muy bien",NOMBRE_ENEMIGO, "tiene de vida:",vidaActualEnemigo);
+            vidaActualEnemigo=VIDA_MAXIMA_ENEMIGA-DANIO_AL_ENEMIGO_POR_HECHIZO
+            vidaActualEnemigo-DANIO_AL_ENEMIGO_POR_HECHIZO
+            console.log("Lo estas haciendo muy bien.",NOMBRE_ENEMIGO, "tiene de vida:",vidaActualEnemigo);
         } else {
-            vidaActualEstudiante = VIDA_MAXIMA_ESTUDIANTE - DAÑO_X_TURNO_AL_ESTUDIANTE
-            console.log("Tu vida actual es:", vidaActual, "recibiste un daño de:", DAÑO_X_TURNO_AL_ESTUDIANTE);
+            vidaActualEstudiante = VIDA_MAXIMA_ESTUDIANTE - DANIO_X_TURNO_AL_ESTUDIANTE
+            console.log("Tu vida actual es:", vidaActual, "recibiste un daño de:", DANIO_X_TURNO_AL_ESTUDIANTE);
         }
     }
 }
