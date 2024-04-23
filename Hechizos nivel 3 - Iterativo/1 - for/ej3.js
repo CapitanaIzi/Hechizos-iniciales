6f1dddfd20109e0ddfe95437b4c66f8da3d2a52b
Francesca <guizarfrancesca7@gmail.com>
2024-04-23 18:20:55 -0300

    empezando

--- Hechizos nivel 3 - Iterativo/1 - for/ej3.js
+++ Hechizos nivel 3 - Iterativo/1 - for/ej3.js
@@ -2,7 +2,12 @@ const leer = require("prompt-sync")();
 
 /**
  * Ej 3
- * La preparación de pociones es una habilidad esencial enseñada en la clase de Pociones de Hogwarts. Cada poción requiere una combinación precisa de ingredientes y una ejecución exacta de los pasos para ser exitosa. En este ejercicio, nos sumergiremos en la atmósfera de la clase de Pociones, donde los estudiantes aprenderán a preparar una de las pociones más icónicas: la poción Multijugos. Para preparar esta poción, los estudiantes deben seguir la receta exacta y medir cuidadosamente las cantidades de cada ingrediente. Cada ingrediente tiene un límite de intentos definido, y solo si se logra ingresar la cantidad correcta del ingrediente previo, el estudiante puede avanzar al siguiente. Si el estudiante no logra ingresar la cantidad correcta de un ingrediente dentro de los intentos permitidos, la preparación de la poción fallará y el proceso se verá interrumpido. Solo aquellos estudiantes que sean precisos y persistentes podrán dominar la preparación de la poción Multijugos y disfrutar de sus efectos transformadores.
+ * La preparación de pociones es una habilidad esencial enseñada en la clase de Pociones de Hogwarts. Cada poción requiere una combinación precisa de ingredientes y una ejecución exacta 
+ * de los pasos para ser exitosa. En este ejercicio, nos sumergiremos en la atmósfera de la clase de Pociones, donde los estudiantes aprenderán a preparar una de las pociones más icónicas:
+ *  la poción Multijugos. Para preparar esta poción, los estudiantes deben seguir la receta exacta y medir cuidadosamente las cantidades de cada ingrediente. Cada ingrediente tiene un límite
+ *  de intentos definido, y solo si se logra ingresar la cantidad correcta del ingrediente previo, el estudiante puede avanzar al siguiente. Si el estudiante no logra ingresar la cantidad
+ *  correcta de un ingrediente dentro de los intentos permitidos, la preparación de la poción fallará y el proceso se verá interrumpido. Solo aquellos estudiantes que sean precisos y 
+ * persistentes podrán dominar la preparación de la poción Multijugos y disfrutar de sus efectos transformadores.
  * 
  * Receta de la Poción Multijugos:
  * Ingredientes:
@@ -20,9 +25,26 @@ const leer = require("prompt-sync")();
     Deja que la poción repose durante 5 minutos antes de su uso.
  * 
  */
-
+const NOMBRE_1_INGREDIENTE="sanguijuelas reventadas"
+const NOMBRE_2_INGREDIENTE="pulverizado de cuerno de Bicornio"
+const NOMBRE_3_INGREDIENTE="pelo de gato negro"
+const NOMBRE_4_INGREDIENTE="colas de serpiente"
+const CANTIDAD_UNIDADES_2_INGREDIENTE= 5
+const MSJ_DE_BIENVENIDA=`los estudiantes deben seguir la receta exacta y medir cuidadosamente las cantidades de cada ingrediente. Cada ingrediente tiene un límite de intentos definido, y solo si se logra ingresar la cantidad correcta del ingrediente previo, el estudiante puede avanzar al siguiente. Si el estudiante no logra ingresar la cantidad correcta de un ingrediente dentro de los intentos permitidos, la preparación de la poción fallará y el proceso se verá interrumpido.`
+const PASO_1_INTRUCCIONES= "Agrega las 3 sanguijuelas reventadas a la caldera."
+const PASO_2_INTRUCCIONES="Agita la mezcla lentamente durante 30 segundos en dirección horaria."
+const PASO_3_INTRUCCIONES="Incorpora las 5 unidades de pulverizado de cuerno de Bicornio y remueve con la cuchara de palo."
+const PASO_4_INTRUCCIONES="Añade el pelo de gato negro y mezcla suavemente en sentido antihorario durante 1 minuto."
+const PASO_5_INTRUCCIONES="Por último, agrega las 2 colas de serpiente y revuelve vigorosamente durante 2 minutos."
+const PASO_6_INTRUCCIONES="Deja que la poción repose durante 5 minutos antes de su uso."
 
 function main() {
+   console.log(MSJ_DE_BIENVENIDA);
+   for (let i = 0; i <0 ; i++) {
+    
+      
+   }
+
 
 }
 
